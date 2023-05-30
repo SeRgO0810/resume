88a941b9f1d15f840c8c53f6559128b5e1a8fd65
SeRgO0810 <steam.1999.guard@gmail.com>
2023-05-30 15:59:20 +0300

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,35 @@ const router = express.Router()
 
 // ================================================================
 
+var header = {
+  name: {
+    firstname: 'Dmytro',
+    lastname: 'Ivanov',
+  },
+  position: 'Junior Fullstack JS Developer',
+  salary: '600$',
+  address: 'м.Вінниця...',
+}
+
+var footer = {
+  social: {
+    email: {
+      text: 'dmytro@mail.com',
+      href: 'mailto:dmytro@mail.com',
+    },
+    phone: {
+      text: '+380670000123',
+      href: 'tel:+380670000123',
+    },
+    linkedIn: {
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/dmytro-test',
+    },
+  },
+}
+
+// ================================================================
+
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
@@ -23,6 +52,228 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Summary',
+    },
+
+    header,
+
+    main: {
+      summary: {
+        title: 'Summary',
+        text: `Open-minded for new technologies, with 1 years of
+        experience in development. Whenever I start to
+        work on a new project I learn the domain and try
+        to understand the idea of the project. Good team
+        player, every colleague is a friend to me.`,
+      },
+
+      experience: {
+        title: 'Other experience',
+        text: `Pet project for parsing sport betting data from
+        different platforms ( odds ) and sport statistics
+        ( tournament position, goals etc), analyzing by
+        simple mathematics models and preparing
+        probability for such events like: money line -
+        first win / draw / second win, totals etc.`,
+      },
+    },
+
+    footer,
+  })
+})
+
+// ================================================================
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Skills',
+    },
+
+    header,
+
+    main: {
+      skills: [
+        {
+          name: 'HTML',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'Handlebars',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'VS Code',
+          point: 8,
+          isTop: false,
+        },
+        {
+          name: 'Git',
+          point: 9,
+        },
+        {
+          name: 'Terminal',
+          point: 9,
+        },
+        {
+          name: 'NPM',
+          point: 9,
+        },
+        {
+          name: 'React.js',
+          point: 0,
+        },
+        {
+          name: 'PHP',
+          point: null,
+        },
+      ],
+      hobbies: [
+        {
+          name: 'Music',
+          isMain: true,
+        },
+        {
+          name: 'Sport',
+          isMain: true,
+        },
+        {
+          name: 'Video Games',
+          isMain: false,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/education', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('education', {
+    // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Education',
+    },
+
+    header,
+
+    main: {
+      educations: [
+        {
+          name: 'Школа',
+          isEnd: true,
+        },
+        {
+          name: 'Ліцей',
+          isEnd: true,
+        },
+        {
+          name: 'Коледж',
+          isEnd: true,
+        },
+        {
+          name: 'Університет',
+          isEnd: false,
+        },
+      ],
+      certificates: [
+        {
+          name: 'Сертивікат про закінчення Школи',
+          id: 12314,
+        },
+        {
+          name: 'Сертивікат про закінчення Ліцею',
+          id: 12314,
+        },
+        {
+          name: 'Сертивікат про закінчення Коледжу',
+          id: 12314,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/work', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('work', {
+    // ↙ сюди вводимо JSON дані
+
+    layout: 'big',
+
+    page: {
+      title: 'Resume | Education',
+    },
+
+    header,
+
+    main: {
+      works: [
+        {
+          position: 'Junior Fullstack Developer',
+          company: {
+            name: 'IT Brains',
+            url: 'https://it-brains.com.ua/',
+          },
+          duration: {
+            from: '10.10.2022',
+            to: null,
+          },
+          projectAmount: 3,
+
+          projects: [
+            {
+              name: 'Resume',
+              url: 'https://www.resonatehq.com/inbound-blog/international-awards-for-it-companies',
+              about:
+                'Airbnb competitor. High-load application for searching apartments',
+              stacksAmount: 3,
+              stacks: [
+                {
+                  name: 'React.js',
+                },
+                {
+                  name: 'HTML / CSS',
+                },
+                {
+                  name: 'Nodejs',
+                },
+              ],
+              awardsAmount: 2,
+              awards: [
+                {
+                  name: 'DataCloud Global Awards. Более 20 различных номинаций, связанных с cloud решениями и дата-центрами.',
+                },
+                {
+                  name: 'IoTGA - Internet of Things Global Awards',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    footer,
   })
 })
 
