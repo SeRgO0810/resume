b095a0cb045c6342eb69f660d5f50e82f0684ea7
SeRgO0810 <steam.1999.guard@gmail.com>
2023-06-08 16:36:03 +0300

    Excursion

--- src/route/index.js
+++ src/route/index.js
@@ -383,5 +383,94 @@ router.get('/person', function (req, res) {
 
 // ================================================================
 
+// ================================================================
+
+router.get('/program', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('program', {
+    layout: 'program',
+
+    program: {
+      excursion: {
+        name: 'Cultural Tour',
+        location: {
+          city: 'Paris',
+          country: 'France',
+        },
+        date: '2023-06-15',
+        program: [
+          {
+            name: 'Museum Visit',
+            type: 'Art',
+            duration: 3,
+            details: {
+              museum: {
+                name: 'The Louvre',
+                location: {
+                  address: 'Rue de Rivoli',
+                  city: 'Paris',
+                  country: 'France',
+                },
+                exhibits: [
+                  {
+                    name: 'Mona Lisa',
+                    artist: 'Leonardo da Vinci',
+                    description: 'Iconic portrait painting',
+                    audio_guide: true,
+                  },
+                  {
+                    name1: 'Winged Victory of Samothrace',
+                    artist1: null,
+                    description1:
+                      'Ancient Greek statue of Nike, the goddess of victory',
+                    audio_guide1: true,
+                  },
+                ],
+              },
+              guide: {
+                name: 'Francois',
+                language: 'French',
+                rating: 4.8,
+              },
+            },
+          },
+          {
+            name1: 'Cultural Show',
+            type1: 'Music and Dance',
+            duration1: 2,
+            details: {
+              venue: {
+                name: 'Moulin Rouge',
+                location: {
+                  address: '82 Boulevard de Clichy',
+                  city: 'Paris',
+                  country: 'France',
+                },
+              },
+              performers: [
+                {
+                  name: 'Mireille Mathieu',
+                  type: 'Chanson singer',
+                },
+                {
+                  name: "Ballet de l'Opéra National de Paris",
+                  type: 'Classical ballet company',
+                },
+              ],
+              guide: {
+                name1: 'Sophie',
+                language1: 'English',
+                rating1: 4.6,
+              },
+            },
+          },
+        ],
+      },
+    },
+  })
+})
+
+// ================================================================
+
 // Підключаємо роутер до бек-енду
 module.exports = router
